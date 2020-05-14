fb2f04302869b24046daac793eb3d948563f9016
ilimarev <limarevigor@gmail.com>
2020-05-14 11:32:59 +0200

    add buttons, calendar and busy/lazy filter

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ from aiohttp import web
 import ssl
 import sys
 
+import datetime
+from telegramcalendar import create_calendar
 import telebot
 bot = telebot.TeleBot('1112147029:AAFdCrgVW7VNIvzrgsG7v7E5G7sPUII35fw');
 
@@ -32,32 +34,49 @@ app.router.add_post("/{token}/", handle)
 jira_options = {'server': 'https://jira.ejaw.net'}
 jira = JIRA(options=jira_options, basic_auth=("jira_bot", "1=20-c_78My/t*fd$8lu//YgutdIO"))
 trusted_users = ['shroombratan', 'Pablito_Po', 'yaroslava_hr', 'maxliulchuk', 'aRe_10']
+current_shown_dates = dict()
+current_options = dict()
+SECONDS_IN_8_HOURS = 28800
 
 
-def send_reports(report_date, message):
+def send_reports(report_date, chat_id, no_report=False, busy=None):
     msg = [f'Daily reports for {report_date} :']
     users = jira.group_members('daily_reports')
     for user in users:
-        msg.append(f'\n{users[user]["fullname"]}')
         issues = jira.search_issues(f'worklogDate={report_date} and worklogAuthor={user}', fields=('worklog',))
-        if not issues:
-            msg.append('has no report')
-        else:
+        if not issues and busy is None:
+            msg.append(f'\n{users[user]["fullname"]}\nhas no report')
+        elif not no_report:
+            if busy is not None:
+                log_seconds = 0
+            log_issue = list()
             for issue in issues:
                 log_time = ''
                 for w in filter(lambda w: w.started[:10] == report_date, issue.fields.worklog.worklogs):
+                    if busy is not None:
+                        log_seconds += w.timeSpentSeconds
                     if log_time == '':
                         log_time = w.timeSpent
                     else:
                         log_time = log_time + ' + ' + w.timeSpent
-                msg.append(f'{issue.key} - time: {log_time}')
+                log_issue.append(f'{issue.key} - time: {log_time}')
+            if busy is None:
+                msg.extend(log_issue)
+            elif busy:
+                if log_seconds >= SECONDS_IN_8_HOURS:
+                    msg.append(f'\n{users[user]["fullname"]}')
+                    msg.extend(log_issue)
+            else:
+                if log_issue and log_seconds < SECONDS_IN_8_HOURS:
+                    msg.append(f'\n{users[user]["fullname"]}')
+                    msg.extend(log_issue)
 
-    bot.send_message(message.from_user.id, '\n'.join(msg))
+    bot.send_message(chat_id, '\n'.join(msg))
 
 
-def report_details(issue_key, message):
-    current_issue = jira.issue(issue_key)
-    msg = [f'Report details for {issue_key} :']
+def get_report_details(message):
+    current_issue = jira.issue(message.text)
+    msg = [f'Report details for {message.text} :']
     for w in current_issue.fields.worklog.worklogs:
         msg.extend((f'{w.author.name} : {w.timeSpent}', w.comment, '----------------------------'))
     bot.send_message(message.from_user.id, '\n'.join(msg))
@@ -67,14 +86,86 @@ def report_details(issue_key, message):
 def start(message):
     if message.text == '/start':
         if message.from_user.username in trusted_users:
-            bot.send_message(message.from_user.id, "Ок, поехали, тебе я доверяю")
-            bot.send_message(message.from_user.id, "Чаво нада, хазяин?")
-            bot.register_next_step_handler(message, get_command)
+            markup = telebot.types.InlineKeyboardMarkup()
+            markup.row(telebot.types.InlineKeyboardButton(text="rep_busy", callback_data="rep_busy"),
+                telebot.types.InlineKeyboardButton(text="rep_lazy", callback_data="rep_lazy"))
+            markup.row(telebot.types.InlineKeyboardButton(text="rep_empty", callback_data="rep_empty"),
+                telebot.types.InlineKeyboardButton(text="daily_report", callback_data="daily_report"))
+            markup.row(telebot.types.InlineKeyboardButton(text="report_details", callback_data="report_details"))
+
+            bot.send_message(message.chat.id, 'Ок, поехали, тебе я доверяю\nЧаво нада, хазяин?', reply_markup=markup)
         else:
             bot.send_message(message.from_user.id, "Наш Гендальф тебя дальше не пустит" +
                              b'\xF0\x9F\x9A\xB7'.decode('utf-8'))
     else:
         bot.send_message(message.from_user.id, 'Напиши /start')      
+
+@bot.callback_query_handler(lambda query: query.data == "report_details")
+def options_callback(query):
+    bot.send_message(query.message.chat.id, "Введи номер таски")
+    bot.register_next_step_handler(query.message, get_report_details)
+
+@bot.callback_query_handler(lambda query: query.data in ["rep_busy", "rep_lazy", "rep_empty", "daily_report"])
+def options_callback(query):
+    chat_id = query.message.chat.id
+    current_options[chat_id] = query.data
+    now = datetime.datetime.now()
+    date = (now.year, now.month)
+    current_shown_dates[chat_id] = date
+    markup = create_calendar(now.year, now.month)
+    bot.send_message(query.message.chat.id, "Пожалуйста, выберите дату", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: 'DAY' in call.data[0:13])
+def handle_day_query(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    last_sep = call.data.rfind(';') + 1
+
+    if saved_date is not None:
+        day = call.data[last_sep:]
+        if len(day) == 1:
+            day = f'0{day}'
+        month = saved_date[1]
+        if month < 10:
+            month = f'0{month}'
+
+        date = f'{saved_date[0]}-{month}-{day}'
+        option = current_options[chat_id]
+        if option == 'daily_report':
+            send_reports(date, chat_id)
+        elif option == 'rep_empty':
+            send_reports(date, chat_id, no_report=True)
+        elif option == 'rep_busy':
+            send_reports(date, chat_id, no_report=False, busy=True)
+        elif option == 'rep_lazy':
+            send_reports(date, chat_id, no_report=False, busy=False)
+        bot.answer_callback_query(call.id, text="")
+
+@bot.callback_query_handler(func=lambda call: 'MONTH' in call.data)
+def handle_month_query(call):
+    info = call.data.split(';')
+    month_opt = info[0].split('-')[0]
+    year, month = int(info[1]), int(info[2])
+    chat_id = call.message.chat.id
+
+    if month_opt == 'PREV':
+        month -= 1
+
+    elif month_opt == 'NEXT':
+        month += 1
+
+    if month < 1:
+        month = 12
+        year -= 1
+
+    if month > 12:
+        month = 1
+        year += 1
+
+    date = (year, month)
+    current_shown_dates[chat_id] = date
+    markup = create_calendar(year, month)
+    bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
 
 def get_command(message):
     if 'daily_report' in message.text and message.text != 'daily_report':
